a3625ca0f8e35a72ed831745e9ad91ab62174044
niccolotubini <tubini.niccolo@gmail.com>
2018-11-14 15:13:16 +0100

    Second commit added some greetings

--- src/org/geoframe/first/HelloWorld.java
+++ src/org/geoframe/first/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("ciao");
+		System.out.println("niao");
+		System.out.println("hallo");
+
 	}
 
 }
